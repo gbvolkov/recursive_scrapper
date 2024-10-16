98706d7633a6041b90ed07edd7685f7b310bc572
gbvolkov <gbvolkov@gmail.com>
2024-10-16 13:38:18 +0300

    1

---
+++ test.py
@@ -0,0 +1,17 @@
+import pandas
+
+pd1 = pandas.read_csv('./output/articles_data_summ.csv', encoding="utf-8")
+pd2 = pandas.read_csv('./output/kb.csv', encoding="utf-8")
+
+#pd1 = pd1[pd1['refs'].notnull()]
+#pd2 = pd2[pd2['refs'].notnull()]
+
+#pd2['url'] = ''
+
+pd = pandas.concat([pd1, pd2], ignore_index=True, sort=False)
+
+pd['no'] = range(1, len(pd) + 1)
+
+print(pd.info())
+
+pd.to_csv('./output/kb_new.csv', index=False, encoding="utf-8") 
